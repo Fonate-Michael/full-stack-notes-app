720e3559fa1ca1c0917b1bcb243c07f7af5bbf7e
AthanaseAlto <altondangang69@gmail.com>
2025-05-05 00:11:41 +0100

    new-console

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-const fetchData = async()=>{
-    const url = "http://127.0.0.1:8000/api/note/"
-    const data = await fetch(url)
-    const response = await data.json()
-    console.log(response)
-}
-console.log("hehehe")
-fetchData()
-
-
-
+const fetchData = async () => {
+  const url = 'http://127.0.0.1:8000/api/note/';
+  const data = await fetch(url);
+  const response = await data.json();
+  console.log(response);
+};
+console.log('hehehe');
+console.log('Michael is a fool!!!');
+fetchData();

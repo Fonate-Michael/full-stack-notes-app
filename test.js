4ff920095ed87c9fe1813d946d2d381f49731ec5
Fonate-Michael <developerfonate@gmail.com>
2025-05-05 00:01:37 +0100

    added the test.js file to make sure no cors issues

---
+++ test.js
@@ -0,0 +1,11 @@
+const fetchData = async()=>{
+    const url = "http://127.0.0.1:8000/api/note/"
+    const data = await fetch(url)
+    const response = await data.json()
+    console.log(response)
+}
+
+fetchData()
+
+
+
